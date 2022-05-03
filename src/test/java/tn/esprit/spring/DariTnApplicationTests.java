0e29739806d91976e06902a708dce361e7351547
FatmaDaas <fatma.daas@esprit.tn>
2022-05-04 00:34:29 +0200

    testm

--- src/test/java/tn/esprit/spring/DariTnApplicationTests.java
+++ src/test/java/tn/esprit/spring/DariTnApplicationTests.java
@@ -1,7 +1,6 @@
 package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
-<<<<<<< HEAD
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,16 +31,4 @@ class DariTnApplicationTests {
 	}
 	
 	
-	
-=======
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DariTnApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
->>>>>>> annoncee
 }
